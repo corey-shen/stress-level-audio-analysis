788f12ace4c9b465d90c96d86eaa053c1620aae6
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-02-15 00:08:33 -0800

    Setting up backend

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # Allow frontend to talk to backend
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Flask background."})
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello from Flask!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
